5e272e7efb9d159910fa3782afbd2a068c0ce44c
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2014-12-03 01:15:34 -0300

    Starting to add mydb support with socket.io

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
  * Module dependencies.
  */
 
-var http = require('http');
-var express = require('express');
-var app = module.exports = require('lib/boot');
+var server = module.exports = require('lib/boot');
+var config = require('lib/config');
 var debug = require('debug')('njs');
 
 /**
  * Launch server
  */
 
-http.createServer(app).listen(app.get('port'), function() {
-  debug('Application started on port %d', app.get('port'));
+server.listen(config('port'), function() {
+  debug('Application started on port %d', config('port'));
 });
